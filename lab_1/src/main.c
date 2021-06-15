8279cfe52006d7a3ed6bb1ba6c99fb09fe81583d
ArtyomAdov <death1angel1annushka1@gmail.com>
2021-06-15 11:21:07 +0700

    Fix code style

--- lab_1/src/main.c
+++ lab_1/src/main.c
@@ -10,30 +10,30 @@
 
 void Print_Integer_Value_By_Bytes(int integer_value)
 {
-    int value_one_byte = 0b0;
-    for(short int i = 0; i < QUANTITY_BYTE_IN_INTEGER_VALUE; i++)
-    {
-        value_one_byte = ((integer_value >> (i << QUANTITY_SHIFT_BIT_FOR_MULTIPLE_EIGHT)) & VALUE_FOR_SAVE_LAST_BYTE);
-        printf("%d) %02X\n", i, value_one_byte);
-    }
+	int value_one_byte = 0;
+	for(short int i = 0; i < QUANTITY_BYTE_IN_INTEGER_VALUE; i++)
+	{
+		value_one_byte = ((integer_value >> (i << QUANTITY_SHIFT_BIT_FOR_MULTIPLE_EIGHT)) & VALUE_FOR_SAVE_LAST_BYTE);
+		printf("%d) %2X\n", i, value_one_byte);
+	}
 }
 
 void Change_Choice_Byte(int* integer_value, int new_value_byte, int number_change_byte)
 {
-    int temp = 0;
-    temp = ~(VALUE_FOR_SAVE_LAST_BYTE << (number_change_byte << QUANTITY_SHIFT_BIT_FOR_MULTIPLE_EIGHT));
-    *integer_value = (*integer_value & temp);
-    temp = (new_value_byte << (number_change_byte << QUANTITY_SHIFT_BIT_FOR_MULTIPLE_EIGHT));
-    *integer_value = (*integer_value | temp);
-} 
+	int temp = 0;
+	temp = ~(VALUE_FOR_SAVE_LAST_BYTE << (number_change_byte << QUANTITY_SHIFT_BIT_FOR_MULTIPLE_EIGHT));
+	*integer_value = (*integer_value & temp);
+	temp = (new_value_byte << (number_change_byte << QUANTITY_SHIFT_BIT_FOR_MULTIPLE_EIGHT));
+	*integer_value = (*integer_value | temp);
+}
 
 int main()
 {
-    int integer_value = START_HEXADECIMAL_VALUE;
-    printf("default value:\n");
-    Print_Integer_Value_By_Bytes(integer_value);
-    CHECK_ERROR_FLAG(Change_Choice_Byte(&integer_value, START_NEW_VALUE_BYTE, START_NUMBER_CHANGE_BYTE));
-    printf("new value:\n");
-    Print_Integer_Value_By_Bytes(integer_value);
-    return 0;
+	int integer_value = START_HEXADECIMAL_VALUE;
+	printf("default value:\n");
+	Print_Integer_Value_By_Bytes(integer_value);
+	Change_Choice_Byte(&integer_value, START_NEW_VALUE_BYTE, START_NUMBER_CHANGE_BYTE);
+	printf("new value:\n");
+	Print_Integer_Value_By_Bytes(integer_value);
+	return 0;
 }
